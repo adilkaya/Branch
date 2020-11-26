7534ded1f23b6eef42ca5ae3cf3657e37f775330
Adil Kaya <@adil.kayaa2@gmail.com>
2020-11-26 12:41:06 +0100

    Sepete ekle 2 kismi yapildi kismi yazildi

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Urunleri listele kismi yazildi");
-        System.out.println("Sepet ekle kismi yapildi kismi yazildi");
+        System.out.println("Yeni Sepete ekle kismi yapildi kismi yazildi");
+        System.out.println("Yeni Sepete ekle 2 kismi yapildi kismi yazildi");
     }
 }
